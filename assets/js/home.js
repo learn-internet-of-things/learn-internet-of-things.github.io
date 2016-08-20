478ae6b6d426076b5f787f3aafe3a8c3d5619bfc
Lausi95 <tomlausmann@gmail.com>
2016-08-20 14:07:20 +0200

    Automatically generated Content Navigation

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,4 @@
+
 $(function() {
     $(document).tooltip({
         position: {
@@ -10,3 +11,26 @@ $(function() {
 $(function() {
     $('map').imageMapResize();
 });
+
+$(function() {
+    buildScrollingContentNavigation();
+});
+
+function buildScrollingContentNavigation() {
+    var index = 0;
+    var sidenav = "";
+    $("h2").each(function() {
+        setAnchor($(this), index);
+        sidenav += buildLink(index, $(this).html());
+    });
+    $("#sliding-content-nav .panel-body").html(sidenav);
+    $("#sliding-content-nav").stick_in_parent({offset_top: 15});
+}
+
+function setAnchor(element, id) {
+    element.attr("id", "anchor-" + id);
+}
+
+function buildLink(index, text) {
+    return "<a href='#anchor-" + index + "'>" + text + "</a><br />";
+}
